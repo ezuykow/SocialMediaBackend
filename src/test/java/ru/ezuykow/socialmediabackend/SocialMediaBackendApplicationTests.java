525baf682d94318af77c297838ff057e364ed8ac
ezuykow <zuy1996@gmail.com>
2023-08-28 18:32:29 +0300

    Initial commit

---
+++ src/test/java/ru/ezuykow/socialmediabackend/SocialMediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ezuykow.socialmediabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
