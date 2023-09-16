2504569045dba4da3526d653a7a4501c8554f81a
ezuykow <zuy1996@gmail.com>
2023-09-16 11:31:15 +0300

    Реализовано удаление друзей

--- src/main/java/ru/ezuykow/socialmediabackend/controllers/FriendsController.java
+++ src/main/java/ru/ezuykow/socialmediabackend/controllers/FriendsController.java
@@ -3,9 +3,7 @@ package ru.ezuykow.socialmediabackend.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.ezuykow.socialmediabackend.services.FriendsService;
 
 /**
@@ -23,6 +21,15 @@ public class FriendsController {
     @GetMapping()
     public ResponseEntity<?> getMyFriends(Authentication auth) {
         return ResponseEntity.ok(friendsService.getMyFriends(auth.getName()));
+    }
+
+    @DeleteMapping()
+    public ResponseEntity<?> deleteFriend(
+            Authentication auth,
+            @RequestParam("target") String username
+    ) {
+        friendsService.deleteFriend(auth.getName(), username);
+        return ResponseEntity.ok().build();
     }
 
     //-----------------API END-------------------
--- src/main/java/ru/ezuykow/socialmediabackend/services/FriendsService.java
+++ src/main/java/ru/ezuykow/socialmediabackend/services/FriendsService.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class FriendsService {
 
     private final UserService userService;
+    private final SubscribeService subscribeService;
     private final UserMapper userMapper;
 
     //-----------------API START-----------------
@@ -25,9 +26,24 @@ public class FriendsService {
         return initiator.getFriends().stream().map(userMapper::mapUserToFriendDto).toList();
     }
 
+    public void deleteFriend(String initiatorUsername, String targetUsername) {
+        User initiator = userService.findUserByUsername(initiatorUsername);
+        User target = userService.findUserByUsername(targetUsername);
+
+        removeFromFriends(initiator, target);
+        subscribeService.removeSubscribe(initiator, target);
+
+        userService.saveAll(List.of(initiator, target));
+    }
+
     public void addToFriends(User fUser, User sUser) {
         fUser.getFriends().add(sUser);
         sUser.getFriends().add(fUser);
+    }
+
+    public void removeFromFriends(User fUser, User sUser) {
+        fUser.getFriends().remove(sUser);
+        sUser.getFriends().remove(fUser);
     }
 
     //-----------------API END-------------------
